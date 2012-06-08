f966b4ea23df1db2e83140e13456ab54f740ad5e
Jan Krüger <krueger@modell-aachen.de>
2012-06-08 17:11:49 +0200

    ModacSkin: Release 1.3.5

--- lib/Foswiki/Contrib/ModacSkin.pm
+++ lib/Foswiki/Contrib/ModacSkin.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 our $VERSION = '$Rev: 8536 $';
-our $RELEASE = '1.3.4';
+our $RELEASE = '1.3.5';
 our $SHORTDESCRIPTION = 'Modell Aachen Skin (Mediawiki New Skin)';
 
 1;
