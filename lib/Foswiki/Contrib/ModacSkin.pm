ac395be5bd7062c96155ffb3b54e82ea99521087
Jan Krüger <krueger@modell-aachen.de>
2012-07-17 11:15:16 +0200

    Release 1.3.8

--- lib/Foswiki/Contrib/ModacSkin.pm
+++ lib/Foswiki/Contrib/ModacSkin.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 our $VERSION = '$Rev: 8536 $';
-our $RELEASE = '1.3.7a';
+our $RELEASE = '1.3.8';
 our $SHORTDESCRIPTION = 'Modell Aachen Skin (Mediawiki New Skin)';
 
 1;
