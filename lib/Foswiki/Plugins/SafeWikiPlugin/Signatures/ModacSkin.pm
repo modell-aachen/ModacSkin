8a5498099dcbc58fb15504d630f8cafac09f19eb
Stephan Osthold <osthold@modell-aachen.de>
2013-01-25 18:22:15 +0100

    Signatur für JavaScript bei leeren Links

--- lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/ModacSkin.pm
+++ lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/ModacSkin.pm
@@ -1,4 +1,5 @@
 # This contains signatures for inline JS code contained in ModacSkin.
 [
     'dpwb+q8ST9yFaglHdRnQ/BjJ+uTuK5GngjhRsPHyVpY', # ModacAjaxHelper
+    'Deekn20h+++EarpL0nFQLX7JSJv7s/2W9f988ZFAh14', # void(0)
 ];
