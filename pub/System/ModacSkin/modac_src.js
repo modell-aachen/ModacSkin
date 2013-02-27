afbdd078a10a75bffa8fd83eeed61fdac88f9032
Jan Krüger <krueger@modell-aachen.de>
2013-02-27 10:04:49 +0100

    pattern.js: übernehme gefixte Version in modac.js
    In pattern.js war der Attachment-Counter fehlerhaft implementiert.
    Übertrage alle von uns benötigten Funktionen aus dieser Datei in
    modac.js -- mit Bugfix.

--- pub/System/ModacSkin/modac_src.js
+++ pub/System/ModacSkin/modac_src.js
@@ -1,5 +1,52 @@
 jQuery(function($){
+    // Auto-submit autocomplete fields depending on class
     (function($tosubmit) {
         $tosubmit.on('autocompleteselect', function(e, ui) { $(this).val(ui.item.value).closest('form').submit(); });
     })($('.solrSearchField, .modacAutoSubmit'));
+
+    // Code below copied from pattern.js
+    // (Search-specific stuff removed because we're using SolrSearch anyway)
+
+    // Mark a form step with a hoopy styled character
+    $("div.foswikiFormStep h3").each(
+        function(index, el) {
+            $(el).before(
+                '<span class="foswikiActionFormStepSign">&#9658;</span>'
+                );
+        });
+
+    // Create an attachment counter in the attachment table twisty.
+    $('div.foswikiAttachments')
+        .each(
+            function(index, el) {
+                // Fixed version of what's in pattern.js
+                var count = $(el).find('table.foswikiTable tr').length - 1;
+                var countStr = " <span class='foswikiSmall'>"
+                    + count + "<\/span>";
+                $(el).find('.patternAttachmentHeader').each(
+                    function(index, el) {
+                        $(el).append(countStr);
+                    });
+            }
+        );
+
+    $('input.foswikiFocus').each(
+        function(index, el) {
+            el.focus();
+        });
+
+    $('input.foswikiChangeFormButton').click(
+        function(e) {
+            if (foswiki.Edit)
+                foswiki.Edit.validateSuppressed = true;
+        });
+
+    $('body.patternEditPage input').keydown(
+        function(event) {
+            if(event.keyCode == 13) {
+              return false;
+            }
+        });
+
 });
+
