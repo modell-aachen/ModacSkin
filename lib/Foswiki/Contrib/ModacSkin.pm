f5548929b4a458e3048a4d8823abc2b713831aeb
Stephan Osthold <osthold@modell-aachen.de>
2013-04-03 13:28:56 +0200

    Release 1.3.31

--- lib/Foswiki/Contrib/ModacSkin.pm
+++ lib/Foswiki/Contrib/ModacSkin.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 our $VERSION = '$Rev: 8536 $';
-our $RELEASE = '1.3.30';
+our $RELEASE = '1.3.31';
 our $SHORTDESCRIPTION = 'Modell Aachen Skin (Mediawiki New Skin)';
 
 1;
