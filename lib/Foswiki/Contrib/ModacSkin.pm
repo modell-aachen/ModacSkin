c3688b22361ed931a4d9e81a708698827742f3a3
Jan Krüger <krueger@modell-aachen.de>
2013-04-19 15:35:45 +0200

    Release 1.3.33: NewUserTemplate ins MANIFEST

--- lib/Foswiki/Contrib/ModacSkin.pm
+++ lib/Foswiki/Contrib/ModacSkin.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 our $VERSION = '$Rev: 8536 $';
-our $RELEASE = '1.3.32';
+our $RELEASE = '1.3.33';
 our $SHORTDESCRIPTION = 'Modell Aachen Skin (Mediawiki New Skin)';
 
 1;
