254082411ae94e8abddb647fcfa99b5014106e79
van Hung Dao <dao@modell-aachen.de>
2013-07-30 15:44:06 +0200

    Dialog Seite löschen:Verweise aktualisieren: Topic->Seite

--- lib/Foswiki/Contrib/ModacSkin.pm
+++ lib/Foswiki/Contrib/ModacSkin.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 our $VERSION = '$Rev: 8536 $';
-our $RELEASE = '1.4.18';
+our $RELEASE = '1.4.19';
 our $SHORTDESCRIPTION = 'Modell Aachen Skin (Mediawiki New Skin)';
 
 1;
