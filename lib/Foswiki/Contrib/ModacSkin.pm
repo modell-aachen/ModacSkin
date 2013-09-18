d99ae7e110edb8249d67ace4101c7bcb08092433
van Hung Dao <dao@modell-aachen.de>
2013-09-18 10:05:38 +0200

    Release 1.4.24

--- lib/Foswiki/Contrib/ModacSkin.pm
+++ lib/Foswiki/Contrib/ModacSkin.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 our $VERSION = '$Rev: 8536 $';
-our $RELEASE = '1.4.23';
+our $RELEASE = '1.4.24';
 our $SHORTDESCRIPTION = 'Modell Aachen Skin (Mediawiki New Skin)';
 
 1;
