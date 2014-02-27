107818313435c9735cafc9e58481ca0addb761fd
Jan Krüger <krueger@modell-aachen.de>
2014-02-27 10:50:37 +0100

    JS: localize 'The required form field ... has no value'
    Ticket: none

---
+++ pub/System/ModacSkin/jsi18n.edit.de_src.js
@@ -0,0 +1,4 @@
+jsi18n.add('edit', {
+  "You have not filled out the mandatory form field '[_1]'.": "Sie haben das Pflichtfeld '[_1]' nicht ausgefüllt.",
+  "Please check your input.": "Bitte überprüfen Sie Ihre Eingaben."
+});
