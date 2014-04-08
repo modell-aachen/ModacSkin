ee9edf8bc5f0035f7e325239e9ae0ecbbc51333c
Stephan Osthold <osthold@modell-aachen.de>
2014-04-08 11:24:40 +0200

    Updating French translations
    Ticket: #4249

---
+++ pub/System/ModacSkin/jsi18n.edit.fr_src.js
@@ -0,0 +1,4 @@
+jsi18n.add('edit', {
+  "You have not filled out the mandatory form field '[_1]'.": "Vous n'avez pas rempli le champ de formulaire obligatoire '[_1]'.",
+  "Please check your input.": "S'il vous plaît vérifier votre saisie."
+});
