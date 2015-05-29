8b3558269f3905a02b2653d643d190f24d8b20c8
Stephan Osthold <osthold@modell-aachen.de>
2015-05-29 13:03:09 +0200

    Feature: Warning when no mandatory checkbox selected
    Also changing 'jQuery(...)' to '$(...)' for this code snipped.
    
    Release-Note(user, feature) {
      Warn when no checkbox is selected, but the field is mandatory.
    [...]
      When a mandatory formfield is not filled out, a warning will be
    displayed. Now this feature also works for checkboxes.
    }
    
    Reviewed on release-day.
    
    Ticket: #8347

--- pub/System/ModacSkin/jsi18n.edit.de_src.js
+++ pub/System/ModacSkin/jsi18n.edit.de_src.js
@@ -1,4 +1,5 @@
 jsi18n.add('edit', {
   "You have not filled out the mandatory form field '[_1]'.": "Sie haben das Pflichtfeld '[_1]' nicht ausgefüllt.",
+  "You have not selected any option of the mandatory form field '[_1]'.": "Sie haben keine Option aus dem Pflichtfeld '[_1]' gewählt.",
   "Please check your input.": "Bitte überprüfen Sie Ihre Eingaben."
 });
