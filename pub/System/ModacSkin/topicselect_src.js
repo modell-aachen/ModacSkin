a720afe5ab3d65db8b71bc28a8d71eb9c2267ae0
Jan Krüger <krueger@modell-aachen.de>
2015-10-22 15:26:09 +0200

    Update select2 on WebCreateNewTopic to v4.0 API (WIP)
    Ticket: none
    Release-Note: none

--- pub/System/ModacSkin/topicselect_src.js
+++ pub/System/ModacSkin/topicselect_src.js
@@ -1,5 +1,5 @@
 jQuery(function($) {
-	$('.jqTopicSelect:not(div)').livequery(function(i) {
+	$('.jqTopicSelect:not(div):not(.jqTopicSelectInited)').livequery(function(i) {
 		var opts = {
 			pagesize: 20,
 			include_web: true,
@@ -8,19 +8,28 @@ jQuery(function($) {
 		};
 		$.extend(opts, $(this).metadata());
 
+		var formatEntry = function(d) {
+			var $e = $('<div class="topicselect_container"><div class="topicselect_label"></div><div class="topicselect_sublabel"></div></div>');
+			$e.find('.topicselect_label').text(d.label);
+			$e.find('.topicselect_sublabel').text(d.sublabel);
+			return $e;
+		};
+		var formatEntryShort = function(d) {
+			return $('<div class="topicselect_label"></div>').text(d.label);
+		};
 		$(this).select2({
 			ajax: {
 				dataType: 'json',
 				url: foswiki.getPreference('SCRIPTURLPATH') +'/view'+ foswiki.getPreference('SCRIPTSUFFIX') +'/System/ModacAjaxHelper',
-				data: function(term, page) {
+				data: function(params) {
 					var offset = 0;
-					if (page > 1) offset = (page - 1);
+					if (params.page > 1) offset = (params.page - 1);
 					return {
 						contenttype: 'text/plain',
 						section: (opts.except ? 'topics_exceptwebs' : 'topicsinweb'),
 						skin: 'text',
 						web: opts.web,
-						query: term.toLowerCase(),
+						query: (params.term||'').toLowerCase(),
 						'offset': offset,
 						count: opts.pagesize,
 						clearable: 1
@@ -37,22 +46,17 @@ jQuery(function($) {
 					};
 				}
 			},
-			formatResult: function(object, container, query) {
-				return '<div class=\"topicselect_label\">'+object.label+'</div><div class=\"topicselect_sublabel\">'+object.sublabel+'</div>';
-			},
-			formatSelection: function(object, container) {
-				return object.sublabel;
-			},
+			templateResult: formatEntry,
+			templateSelection: formatEntryShort,
+			// TODO
 			formatNoMatches: function() { return foswiki.getMetaTag('l10n_modac_selecttopic_nomatches'); },
 			formatSearching: function() { return '<div class=\"jqAjaxLoader\" style=\"padding-left: 20px;\">'+ foswiki.getMetaTag('l10n_modac_selecttopic_searching') +'</div>'; },
 			formatResultCssClass: function() { return 'topicselect_container'; },
-			initSelection: function(element, callback) {
-				var val = $(element).val();
-				callback({id: val, label: val, sublabel: val});
-			},
 			allowClear: false,
+			minimumInputLength: 0,
 			width: 'element'
 		});
+		$(this).addClass('jqTopicSelectInited');
 	});
 });
 
