48bf82d7e4e9f4c44a02199e34b5c91cf87eee38
Brunner <brunner@modell-aachen.de>
2017-01-17 16:08:00 +0100

    Added some general translation for JQTableSorterPlugin.
    Translation was added here, because it is not possible to expand JSI18N macro in JQTableSorterPlugin.
    
    Ticket: none
    id:254 comment: Hier wurde nur die Uebersetzung fuer den Dialog hinzugefuegt.

--- pub/System/ModacSkin/jsi18n.view.de_src.js
+++ pub/System/ModacSkin/jsi18n.view.de_src.js
@@ -3,6 +3,7 @@ jsi18n.add('ModacSkin', {
     "Found references in these topics:": "Referenzen auf diesen Seiten gefunden:",
     "No references found": "Keine Referenzen gefunden",
     "References for [_1]": "Referenzen für [_1]",
+    "It is not possible to activate sorting for tables, that have less headers than sortable columns. We therefore deactivated sorting in your table.": "Es ist nicht möglich die Sortierung für Tabellen zu aktivieren, die weniger Überschriften als sortierbare Spalten haben. Die Sortierung für Ihre Tabelle wurde daher deaktiviert.",
     "": ""
 });
 
